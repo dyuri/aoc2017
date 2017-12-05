27af0c76909c5d2f22fd26465fb1c6e14180ffc6
Gyuri Horak <gyuri@euedge.com>
2017-12-05 13:39:22 +0100

    day 5

---
+++ d5_2/jumps.py
@@ -0,0 +1,16 @@
+JUMPS = []
+
+with open('stack.txt') as f:
+    JUMPS = [int(j.strip()) for j in f.readlines()]
+
+pos = 0
+steps = 0
+
+try:
+    while True:
+        next = JUMPS[pos]
+        JUMPS[pos] += 1 if next < 3 else -1
+        pos = next + pos
+        steps += 1
+except IndexError:
+    print('steps required: %s' % steps)
